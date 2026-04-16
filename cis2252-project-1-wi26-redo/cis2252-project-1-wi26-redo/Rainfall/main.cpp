246bc450fd89e95209a5381dd0c6de443adc61de
anthonylang06-ui <lang.anthony@student.oaklandcc.edu>
2026-04-15 23:07:14 -0400

    finished all but build still fails

--- cis2252-project-1-wi26-redo/cis2252-project-1-wi26-redo/Rainfall/main.cpp
+++ cis2252-project-1-wi26-redo/cis2252-project-1-wi26-redo/Rainfall/main.cpp
@@ -9,22 +9,35 @@
 using namespace std;
 int main()
 {
+    double weight;
+    double height;
+    double bmi;
+
+    cout << "Enter weight: ";
+    cin >> weight;
+
+    cout << "Enter height: ";
+    cin >> height;
+
+    bmi = weight * 703 / (height * height);
+
+    cout << "BMI is " << bmi << endl;
+
+    if (bmi < 18.5)
+    {
+        cout << "Underweight" << endl;
+    }
+
+    if (bmi >= 18.5 && bmi <= 25)
+    {
+        cout << "Optimal weight" << endl;
+    }
+
+    if (bmi > 25)
+    {
+        cout << "Overweight" << endl;
+    }
+
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    return 0
+    return 0;
 }
