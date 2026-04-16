8dca88fb4cf4dc2efff9b86d9f07318f177a746a
anthonylang06-ui <lang.anthony@student.oaklandcc.edu>
2026-04-15 22:22:27 -0400

    finished pennies

--- cis2252-project-1-wi26-redo/cis2252-project-1-wi26-redo/Pennies/main.cpp
+++ cis2252-project-1-wi26-redo/cis2252-project-1-wi26-redo/Pennies/main.cpp
@@ -5,25 +5,37 @@
 //  Created by Anthony Lang on 4/15/26.
 //
 #include <iostream>
-#include <string>
+#include <iomanip>
 using namespace std;
 int main()
 {
-  
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    return 0
+    int days;
+    int day;
+    double pay;
+    double total;
+
+    pay = 0.01;
+    total = 0;
+
+    cout << "Enter days worked: ";
+    cin >> days;
+
+    while (days < 1 || days > 31)
+    {
+        cout << "Invalid. Enter days worked: ";
+        cin >> days;
+    }
+
+    cout << fixed << setprecision(2);
+
+    for (day = 1; day <= days; day++)
+    {
+        cout << "Day " << day << ": $" << pay << endl;
+
+        total = total + pay;
+        pay = pay * 2;
+    }
+
+    cout << "Total: $" << total << endl;
+    return 0;
 }
