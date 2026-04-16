a1b6e3c5efa042e52558fa0219c4cf2caee436e2
anthonylang06-ui <lang.anthony@student.oaklandcc.edu>
2026-04-15 21:36:23 -0400

    finished BMI

--- cis2252-project-1-wi26-redo/cis2252-project-1-wi26-redo/BMI/main.cpp
+++ cis2252-project-1-wi26-redo/cis2252-project-1-wi26-redo/BMI/main.cpp
@@ -18,20 +18,28 @@ int main()
     double rain3;
     double average;
 
+    cout << "Enter first month: ";
+    cin >> month1;
 
+    cout << "Enter rain amount: ";
+    cin >> rain1;
 
+    cout << "Enter second month: ";
+    cin >> month2;
 
+    cout << "Enter rain amount: ";
+    cin >> rain2;
 
+    cout << "Enter third month: ";
+    cin >> month3;
 
+    cout << "Enter rain amount: ";
+    cin >> rain3;
 
+    average = (rain1 + rain2 + rain3) / 3;
 
-
-
-
-
-
-
-
-
-    return 0
+    cout << "The average monthly rainfall for ";
+    cout << month1 << ", " << month2 << ", and " << month3;
+    cout << " was " << average << " inches." << endl;
+    return 0;
 }
